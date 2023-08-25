b7c57d0f35384fff5694448c8c6bddb57233de09
seoksii <125940144+seoksii@users.noreply.github.com>
2023-08-25 10:20:30 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.17 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution {
+    public int solution(int[] numbers) {
+        int answer = 45;
+        foreach (int i in numbers)
+            answer -= i;
+        return answer;
+    }
+}
